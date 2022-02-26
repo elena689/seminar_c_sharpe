4ca2f34e3ae2260070804bd7f89ad1876414d61d
Elena <lesikr@ya.ru>
2022-02-26 20:39:36 +0300

    zadacha8

---
+++ zadacha8/Program.cs
@@ -0,0 +1,15 @@
+﻿int numberA = new int();
+Console.WriteLine("Введите число ");
+numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = 0;
+int index = 1;
+
+while(index <= numberA)
+{
+    numberB = numberA-(numberA-index);
+        if(numberB%2 == 0)
+        {
+            Console.Write(numberB+ " ");
+        }
+   index = index + 1;
+}
