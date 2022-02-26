9847d6b8b14bac1d3fa382bf11cd185b8a4a614a
Elena <lesikr@ya.ru>
2022-02-26 20:27:37 +0300

    zadacha4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,20 @@
+﻿int number_a = new int ();
+int number_b = new int ();
+int number_c = new int ();
+Console.WriteLine("Введите первое число:");
+number_a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+number_b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+number_c = Convert.ToInt32(Console.ReadLine());
+
+int max = number_a;
+
+if(number_a > max ) max = number_a;
+if(number_b > max ) max = number_b;
+if(number_c > max ) max = number_c;
+
+Console.Write("Максимальное число - ");
+Console.WriteLine(max);
