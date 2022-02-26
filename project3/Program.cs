6f37f5d52e2aaa64c5477d135129439942b624a8
Elena <lesikr@ya.ru>
2022-02-26 17:55:01 +0300

    progect3

---
+++ project3/Program.cs
@@ -0,0 +1,7 @@
+﻿int number_a = new int ();
+int number_b = new int ();
+
+Console.WriteLine("Введите число:");
+number_a = Convert.ToInt32(Console.ReadLine());
+number_b = number_a*number_a;
+Console.WriteLine("Квадрат числа " +number_a+ " равен " +number_b);
