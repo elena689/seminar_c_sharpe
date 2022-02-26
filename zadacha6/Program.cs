c64175061e0cd9292961f5d369e042dcb8b32a34
Elena <lesikr@ya.ru>
2022-02-26 18:50:53 +0300

    zadach6

---
+++ zadacha6/Program.cs
@@ -0,0 +1,13 @@
+﻿int number_a = new int ();
+
+Console.WriteLine("Введите число:");
+number_a = Convert.ToInt32(Console.ReadLine());
+
+if(number_a%2 == 0)
+{
+    Console.WriteLine(number_a+ " - чётное число");
+}
+else
+{
+    Console.WriteLine(number_a+ " - нечётное число");
+}
