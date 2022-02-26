6743f9d932f525caf10356a758f8febc7edd3667
Elena <lesikr@ya.ru>
2022-02-26 20:24:46 +0300

    project4

---
+++ project4/Program.cs
@@ -0,0 +1,13 @@
+﻿int numberA = new int();
+Console.WriteLine("Введите число ");
+numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = 0;
+int index = 0;
+
+while(index <= numberA*2)
+{
+        numberB = numberA-(2*numberA-index);
+        Console.Write(numberB+ " ");
+   index = index + 1;
+}
+
