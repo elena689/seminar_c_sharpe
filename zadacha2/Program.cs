15b3b6f4ec56aa8a21dee3751739703636bc519b
Elena <lesikr@ya.ru>
2022-02-26 18:08:55 +0300

    zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,16 @@
+﻿int number_a = new int ();
+int number_b = new int ();
+Console.WriteLine("Введите первое число:");
+number_a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+number_b = Convert.ToInt32(Console.ReadLine());
+
+if (number_a > number_b)
+{
+    Console.WriteLine("Max = " +number_a+ ", Min = " +number_b);
+}
+else 
+{
+Console.WriteLine("Max = " +number_b+ ", Min = " +number_a);
+}
