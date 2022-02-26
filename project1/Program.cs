a4971e90af2a262f7a292f2f55cc6a1017163564
Elena <lesikr@ya.ru>
2022-02-26 17:54:22 +0300

    progect1

--- project1/Program.cs
+++ project1/Program.cs
@@ -1,6 +1,16 @@
-﻿int number = new int ();
-Console.WriteLine("Введите число:");
+﻿int number_a = new int ();
+int number_b = new int ();
+Console.WriteLine("Введите первое число:");
+number_a = Convert.ToInt32(Console.ReadLine());
 
-number = Convert.ToInt32(Console.ReadLine());
-number = number * number;
-Console.WriteLine(number);
+Console.WriteLine("Введите второе число:");
+number_b = Convert.ToInt32(Console.ReadLine());
+int number_c = number_a*number_a;
+if (number_c==number_b)
+{
+    Console.WriteLine("Первое число является квадратом второго числа");
+}
+else 
+{
+Console.WriteLine("Первое число не является квадратом второго числа");
+}
