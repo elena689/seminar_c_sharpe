f6da149e1b4ca83435d87106556f645ac9b3c687
Elena <lesikr@ya.ru>
2022-02-26 17:54:42 +0300

    progect2

---
+++ project2/Program.cs
@@ -0,0 +1,34 @@
+﻿int day = new int ();
+
+while (true)
+{
+    Console.WriteLine("Введите число дня недели от 1 до 7:");
+    day = Convert.ToInt32(Console.ReadLine());
+    switch (day)
+    {
+        case 1:
+            Console.WriteLine("Понедельник");
+            break;
+        case 2:
+            Console.WriteLine("Вторник");
+            break;
+        case 3:
+            Console.WriteLine("Среда");
+            break;
+        case 4:
+            Console.WriteLine("Четверг");
+            break;
+        case 5:
+            Console.WriteLine("Пятница");
+            break;
+        case 6:
+            Console.WriteLine("Суббота");
+            break;
+        case 7:
+            Console.WriteLine("Воскресение");
+            break;
+        default:
+            Console.WriteLine("Нет дня недели для этого числа");
+            break;
+    }
+}
